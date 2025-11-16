0a7a6fdd55f1909f47bff085ae81d91e70b794d8
aasjos <joshiaasmi17@gmail.com>
2025-11-16 17:27:14 -0600

    Added name to UI

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>Created by Aasmi Joshi</h2>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
